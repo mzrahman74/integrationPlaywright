36244d570b375203bff766d25bdd12ef76628174
Mohammad Ziaur Rahman <mohammadziaurrahman@Mohammads-iMac.local>
2025-11-22 12:42:18 -0600

    test cases updated

--- tests/web.spec.ts
+++ tests/web.spec.ts
@@ -1,5 +1,22 @@
 import { test, expect } from "@playwright/test";
 
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://webgopros.com");
+});
+
+test("verify home page", async ({ page }) => {
+  await expect(page.locator(".logo")).toBeVisible();
+  await expect(page.locator("video.responsive_video")).toBeVisible();
+  await expect(page.locator("iframe.video_one")).toBeVisible();
+  const frame = page.locator("iframe.video_one");
+
+  await expect(frame).toHaveAttribute(
+    "src",
+    "https://www.youtube.com/embed/L7dAYld_7aM"
+  );
+  await expect(page.locator("iframe.video_two")).toHaveId("video");
+});
+
 test("@web confirm about page assertion", async ({ page }) => {
   const viewportSize = await page.evaluate(() => {
     return {
@@ -7,7 +24,7 @@ test("@web confirm about page assertion", async ({ page }) => {
       height: window.innerHeight,
     };
   });
-  await page.goto("https:webgopros.com");
+
   if (viewportSize.width <= 991) {
     await page.locator(".navbar-toggler-icon").click();
     await page.locator('a[href="/about"]').click();
